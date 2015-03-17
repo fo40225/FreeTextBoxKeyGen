20922e9b37b7073cbacf3baec463dc386c098be5
fo40225 <fo40225@hotmail.com>
2015-03-17 22:42:10 +0800

    First Commit

---
+++ FreeTextBoxKeyGen/FrmMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FreeTextBoxKeyGen
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FreeTextBox 3.3.1 Distribution License";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "License To: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(82, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 57);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "KeyGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
---
+++ FreeTextBoxKeyGen/FrmMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FreeTextBoxKeyGen
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private readonly byte[] encryptionKeyBytes = new byte[] { 57, 72, 66, 50, 56, 49, 70, 54 };
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                return;
+            }
+            string Name = txtName.Text;
+
+            int intNameLength = Name.Length;
+
+            string strNameLength = intNameLength.ToString();
+
+            while (strNameLength.Length < 5)
+            {
+                strNameLength = strNameLength.Insert(0, "0");
+            }
+
+            string PlainText = strNameLength + Name;
+            ASCIIEncoding aSCIIEncoding = new ASCIIEncoding();
+            byte[] PlainArray = aSCIIEncoding.GetBytes(PlainText);
+
+            DESCryptoServiceProvider dESCryptoServiceProvider = new DESCryptoServiceProvider();
+            ICryptoTransform transform = dESCryptoServiceProvider.CreateEncryptor(encryptionKeyBytes, encryptionKeyBytes);
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+
+            string CipherText = string.Empty;
+
+            if (PlainArray.Length % 24 == 0)
+            {
+                cryptoStream.Write(PlainArray, 0, PlainArray.Length);
+                CipherText = Convert.ToBase64String(memoryStream.ToArray());
+            }
+            else
+            {
+                cryptoStream.Write(PlainArray, 0, PlainArray.Length);
+                cryptoStream.Write(PlainArray, 0, PlainArray.Length);
+                int byteCount = (PlainArray.Length / 24 + 1) * 24;
+                CipherText = Convert.ToBase64String(memoryStream.ToArray().Take(byteCount).ToArray());
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("MyTextBox License");
+            sb.AppendLine("[qeUPffrT7QkE0JgbUKQPxMiBFyyieNJhQXflRmfRui0=]");
+            sb.Append("[");
+            sb.Append(CipherText);
+            sb.Append("]");
+
+            File.WriteAllText("FreeTextBox.lic", sb.ToString(), Encoding.ASCII);
+        }
+    }
+}
